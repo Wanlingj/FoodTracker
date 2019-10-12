7ee22c804ca07ce82fb84c6f4186347df4364ff6
Wanling Jiang <jiang267@purdue.edu>
2019-10-12 16:24:37 -0700

    Initial status

---
+++ FoodTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTracker
+//
+//  Created by USER on 10/3/19.
+//  Copyright © 2019 Wanling. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
